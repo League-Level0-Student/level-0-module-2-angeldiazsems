74358bfd78184fb58964bb32d349ed8689fcfec2
Diaz-Sanchez.Angel <Diaz-Sanchez.Angel@SEMS-259-33.SEMS259.LAB>
2020-02-19 14:51:19 -0800

    eq	ew	we

--- src/_99_extra/checkpoint.java
+++ src/_99_extra/checkpoint.java
@@ -2,32 +2,50 @@ package _99_extra;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class checkpoint {
 public static void main(String[] args) {
 
 	
+
+	// int hi = 0;
 	
-	int hi = 0;
+	//Random hello = new Random();
+	//hi = hello.nextInt(1001); 
+	//System.out.println(hi);
 	
-	Random hello = new Random();
-	hi = hello.nextInt(101); 
+
 	
+
+	//int h = 0;
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	//Random e = new Random();
+	//h = e.nextInt(901); 
+	//System.out.println(h);
 	
 	
 	
 	
 	
+	//int f = 1000;
+	
+//	int j = 900;
+	
+	
+      //f+j
+	
+	
+	
+	
+	
+
+//int a = 1000;
+	
+	//int l = 900;
+	
+	
+	//a-l
 	
 	
 	
@@ -35,15 +53,102 @@ public static void main(String[] args) {
 	
 	
 	
+	//String z = JOptionPane.showInputDialog("How many cups of flour do you have");
+	
+	//int y = Integer.parseInt(z);
+	
+//if (y <2) {
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
+	//JOptionPane.showMessageDialog(null, "You should get more flour");
+
+//}
+
+
+
+
+
+
+
+//String z = JOptionPane.showInputDialog("How many people are you going to give cookies to");
+
+//int y = Integer.parseInt(z);
+
+//if (y >30) {
+
+
+
+//JOptionPane.showMessageDialog(null, "Your going to have to bake 2 batchs of cookies");
+
+//}
+
+
+
+
+
+
+
+
+
+
+
+//String z = JOptionPane.showInputDialog("How many batchs of cookies are you going to make?");
+
+//int y = Integer.parseInt(z);
+
+//if (y == 2) {
+
+
+
+//JOptionPane.showMessageDialog(null, "Your lucky to have so much friends");
+
+//}
+
+
+
+
+
+
+
+
+
+ getNumber();
+//setName("cody");
+
+	//printMessage();
+
+
+
 }
+//static   void printMessage()
+{
+    //System.out.println("message");
+
 }
+
+
+
+	//static void setName(String name) {
+ // String    newName = name;
+
+	
+
+
+	
+ static int getNumber() {
+    return 7;
+} 	
+	
+	
+	
+	
+	} 
+
+
+
+
+
+
+
+
